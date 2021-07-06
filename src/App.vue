d0663b8469f2265853ed4b59d17aba49836bed06
Muzaffer Tolga Yakar <56550130+yakarcoder@users.noreply.github.com>
2021-07-06 23:18:33 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <Navbar />
+  <router-view/>
+</template>
+<script>
+  import Navbar from './components/Navbar.vue'
+  export default{
+    name: "App",
+    components: {Navbar}
+  }
+</script>
+
+<style>
+body{
+  background: #f2f2f2;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  max-width: 600px;
+  margin: 0 auto;
+  color: #555;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/components/SingleProject.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="project" :class="{ complete: project.complete }">
+    <div class="actions">
+      <h3 @click="showDetails = !showDetails">{{ project.title }}</h3>
+      <div class="icons">
+        <router-link :to="{ name: 'EditProject', params: { id: project.id } }">
+          <span class="material-icons">edit</span>
+        </router-link>
+        <span class="material-icons" @click="deleteProject">delete</span>
+        <span class="material-icons tick" @click="toggleComplete">done</span>
+      </div>
+    </div>
+    <div class="details" v-if="showDetails">
+      <p>{{ project.details }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['project'],
+  data(){
+    return{
+      showDetails: false,
+      uri: 'http://localhost:3000/projects/' + this.project.id
+    }
+  },
+  methods:{
+    deleteProject(){
+      fetch(this.uri, { method: 'DELETE' })
+        .then(() => this.$emit('delete', this.project.id))
+        .catch(err => console.log(err))
+    },
+    toggleComplete(){
+      fetch(this.uri, { 
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ complete: !this.project.complete })  
+      }).then(() => this.$emit('complete', this.project.id))
+        .catch(err => console.log(err))
+    },
+  }
+}
+</script>
+
+<style>
+  .project{
+    margin: 20px auto;
+    background: white;
+    padding: 10px 20px;
+    border-radius: 4px;
+    box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.5);
+    border-left: 4px solid #e90074;
+  }
+  h3{
+    cursor: pointer;
+  }
+  .actions{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .material-icons{
+    font-size: 24px;
+    margin-left: 10px;
+    color: #bbb;
+    cursor: pointer;
+  }
+  .material-icons:hover{
+    color: #777;
+  }
+  .project.complete {
+    border-left: 4px solid #00ce89;
+  }
+  .project.complete .tick{
+    color: #00ce89;
+  }
+</style>
+
+{
+  "projects" : [
+    {
+      "id": 1,
+      "title": "Create new homepage banner",
+      "details": "Lorem ispum",
+      "complete": false
+    },
+    {
+      "id": 2,
+      "title": "Make marketing email",
+      "details": "Lorem ispum",
+      "complete": true
+    }
+  ]
+}
